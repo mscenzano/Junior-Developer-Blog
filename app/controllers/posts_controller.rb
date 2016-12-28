5567822a81be1d440289ea9bfdb80b7aac27b5c6
Mariano SDC <mscenzano@gmail.com>
2016-12-28 11:23:34 +0100

    validations for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,15 @@ class PostsController < ApplicationController
   end 
 
   def new
-
+    @post = Post.new
   end 
 
   def create
-    @post = Post.create(post_params)
-    if @post
+    @post = Post.new(post_params)
+    if @post.save
       redirect_to @post
+    else
+      render 'new'
     end
 
   end 
